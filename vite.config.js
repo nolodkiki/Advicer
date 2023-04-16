ae5181fa67040f455084211ee802e6d0c2535f1b
Pavel <pavel.holod2001@gmail.com>
2023-04-16 03:33:36 +0300

    base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://nolodkiki.github.io/Advicer/'
 })
