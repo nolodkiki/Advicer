4916834214ef830f2338b92c0de49c6b18d94f32
Pavel <pavel.holod2001@gmail.com>
2023-04-16 04:23:31 +0300

    base04

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [react()],
-  base: '/Advicer/',
+  base: '/Advicer/src/main.jsx',
   build: {
     outDir: 'build'
   }
