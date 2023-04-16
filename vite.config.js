e13917ef7a72d250c7475cb6d7fc84ebc49670fe
Pavel <pavel.holod2001@gmail.com>
2023-04-16 03:52:56 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/'
+  // plugins: [react()],
+  base: '/Advicer/',
+  build: {
+    outDir: 'build'
+  }
 })
